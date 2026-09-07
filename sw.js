5f4b92f535cc2111f8edd6818290d8e813d05ae0
malawhub <alicamon03@gmail.com>
2026-09-07 17:20:25 +0200

    Cache new MalawiHub video resources page

--- sw.js
+++ sw.js
@@ -1,33 +1,7 @@
-const CACHE_NAME = "malawihub-v4";
-
+const CACHE_NAME = "malawihub-v5";
 const APP_FILES = [
-  "/",
-  "/index.html",
-  "/tutorial/index.html",
-  "/online-class/index.html",
-  "/online-class/admin.html",
-  "/css/style.css",
-  "/js/app.js",
-  "/js/supabase.js",
-  "/manifest.json",
-  "/icons/icon.svg"
+  "/","/index.html","/tutorial/index.html","/online-class/index.html","/online-class/admin.html","/online-class/resources.html","/css/style.css","/js/app.js","/js/supabase.js","/manifest.json","/icons/icon.svg"
 ];
-
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES)));
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(fetch(event.request).then(response => {
-    const copy = response.clone();
-    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-    return response;
-  }).catch(() => caches.match(event.request)));
-});
+self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))); self.skipWaiting(); });
+self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); self.clients.claim(); });
+self.addEventListener("fetch", event => { if(event.request.method!=="GET")return; event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request))); });
