17d451f796bf919e7e38887709aaddc0ca2c2009
malawhub <alicamon03@gmail.com>
2026-09-24 20:10:15 +0200

    Refresh admin app for stable duplex audio

--- android-admin/app/src/main/java/mw/malawihub/admin/MainActivity.java
+++ android-admin/app/src/main/java/mw/malawihub/admin/MainActivity.java
@@ -20,7 +20,7 @@ import android.widget.TextView;
 import org.json.JSONObject;
 
 public class MainActivity extends Activity {
-    private static final String ADMIN_URL = "https://malawihub.pages.dev/admin/login.html?v=20260924-audio-duplex2";
+    private static final String ADMIN_URL = "https://malawihub.pages.dev/admin/login.html?v=20260924-audio-duplex3";
     private WebView webView;
     private ProgressBar progress;
     private android.webkit.ValueCallback<android.net.Uri[]> fileCallback;
